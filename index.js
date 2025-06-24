9440350ff8bcb00fec4db53d3dcbc8975ddb90ee
Ubuntu <ubuntu@ip-172-31-5-97.eu-north-1.compute.internal>
2025-06-24 06:19:11 +0000

    new commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from Node.js app with GitHub Actions CI/CD! hello from stark');
+  res.send('🚀 Hello from Node.js app with GitHub Actions CI/CD! hello from stark pune');
 });
 
 app.listen(port, () => {
